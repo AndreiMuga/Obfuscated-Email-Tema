9be218e47d26fcf1747cde36bbf339a93af951cc
AndreiMuga <andrei.muga@gmail.com>
2024-04-15 19:58:41 +0300

    Push all

---
+++ Obfuscate Email Tema/Obfuscate Email Tema/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+
+class Program
+{
+    static void Main()
+    {
+        string email = "awesome@dotnet.com";
+        Console.WriteLine(EmailCensored(email));
+    }
+    static string EmailCensored(string mail)
+    {
+        string censoredEmail;
+        int atSignIndex = mail.IndexOf('@');
+        string usernameSubstring = mail.Substring(0, atSignIndex);
+        string domainName = mail.Remove(0, atSignIndex);
+        string cesonsoredUsername = new string('*', usernameSubstring.Length);
+        censoredEmail = new string ($"{ cesonsoredUsername}{ domainName}");
+        return censoredEmail;
+
+    }
+}
